32bfeab25d92c4c0d47487cbf285eb21784abf09
Matt Rosno <matt.rosno@gmail.com>
2020-08-03 15:55:50 -0500

    feat(home): prep home page mdx

--- src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
+++ src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import ResourceLinks from 'gatsby-theme-carbon/src/components/LeftNav/ResourceLinks';
+import BadgeForm from "../../../components/BadgeForm";
+import React from "react";
+import ResourceLinks from "gatsby-theme-carbon/src/components/LeftNav/ResourceLinks";
 
 const links = [
   {
-    title: 'Github',
-    href: 'https://github.com/carbon-design-system/gatsby-theme-carbon',
+    title: "GitHub",
+    href: "https://github.com/carbon-design-system/gatsby-theme-carbon",
   },
   {
-    title: 'Carbon',
-    href: 'https://www.carbondesignsystem.com',
-  },
-  {
-    title: 'Configuration guide',
-    href: '/guides/configuration',
+    title: "Carbon",
+    href: "https://www.carbondesignsystem.com",
   },
 ];
 
-// shouldOpenNewTabs: true if outbound links should open in a new tab
-const CustomResources = () => <ResourceLinks shouldOpenNewTabs links={links} />;
+const CustomResources = () => {
+  return (
+    <>
+      <BadgeForm />
+      <ResourceLinks shouldOpenNewTabs links={links} />
+    </>
+  );
+};
 
 export default CustomResources;
